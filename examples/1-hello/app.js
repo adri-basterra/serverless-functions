31fbee04ad7cd712e093ade586b41605d2634db5
adri-st <abasteru@emeal.nttdata.com>
2022-11-24 17:05:48 +0100

    first server function

---
+++ examples/1-hello/app.js
@@ -0,0 +1,15 @@
+const result = document.querySelector('.result');
+
+const fetchData = async () => {
+  console.log('fetchData called');
+  try {
+    const { data } = await axios.get('/.netlify/functions/1-hello');
+    console.log(data);
+    result.textContent = data;
+  }
+  catch (err) {
+    console.error(err.response.data, err.message);
+  }
+}
+
+fetchData();
