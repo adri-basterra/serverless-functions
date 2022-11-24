1da55532be3cdf92ecc3a9bfa99c1a408047b744
adri-st <abasteru@emeal.nttdata.com>
2022-11-24 17:11:58 +0100

    redirections + refactor of .toml

--- examples/1-hello/app.js
+++ examples/1-hello/app.js
@@ -3,7 +3,8 @@ const result = document.querySelector('.result');
 const fetchData = async () => {
   console.log('fetchData called');
   try {
-    const { data } = await axios.get('/.netlify/functions/1-hello');
+    // const { data } = await axios.get('/.netlify/functions/1-hello'); // Before redirection in netlify.toml
+    const { data } = await axios.get('/api/1-hello');
     console.log(data);
     result.textContent = data;
   }
