27e3312bafe913768edb3ac8ce46526de7c44c2c
adri-st <abasteru@emeal.nttdata.com>
2022-11-25 08:30:15 +0100

    basic API function

--- examples/1-hello/app.js
+++ examples/1-hello/app.js
@@ -1,7 +1,6 @@
 const result = document.querySelector('.result');
 
 const fetchData = async () => {
-  console.log('fetchData called');
   try {
     // const { data } = await axios.get('/.netlify/functions/1-hello'); // Before redirection in netlify.toml
     const { data } = await axios.get('/api/1-hello');
