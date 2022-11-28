ff990f52ffc957b28ca0f524692614776ffb43f2
adri-st <abasteru@emeal.nttdata.com>
2022-11-28 11:53:34 +0100

    serverless function

--- functions/6-newsletter.js
+++ functions/6-newsletter.js
@@ -6,6 +6,30 @@ const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
   .table('survey');
 
 exports.handler = async (event, context) => {
+  const method = event.httpMethod;
+  if (method !== 'POST') {
+    return {
+      statusCode: 405,
+      body: "Only POST requests are allowed"
+    }
+  }
+  const { email } = JSON.parse(event.body);
+  try {
+    const data = await axios.post(url, { email }, {
+      headers: {
+        Authorization: `Token ${process.env.NEWSLETTER_KEY}`
+      }
+    });
+    return {
+      statusCode: 201,
+      body: 'Sucess'
+    }
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error.response.data)
+    }
+  }
   return {
     headers: { 'Access-Control-Allow-Origin': '*' },
     statusCode: 200,
