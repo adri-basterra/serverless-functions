4b7d3c41c67654179ecf8c22764af284df902a49
adri-st <abasteru@emeal.nttdata.com>
2022-11-28 10:27:00 +0100

    only post method supported

--- functions/5-weather.js
+++ functions/5-weather.js
@@ -1,10 +1,17 @@
 
 exports.handler = async (event, context) => {
   const method = event.httpMethod;
+  if (method === 'POST') {
+    const { city } = JSON.parse(event.body);
 
+    return {
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      statusCode: 200,
+      body: 'Weather Example'
+    }
+  }
   return {
-    headers: { 'Access-Control-Allow-Origin': '*' },
-    statusCode: 200,
-    body: 'Weather Example'
+    statusCode: 405,
+    body: 'Only POST method is supported'
   }
 }
