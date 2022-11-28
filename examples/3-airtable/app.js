91d39ea463eb5790c6a7aee7e722d8417073fd4c
adri-st <abasteru@emeal.nttdata.com>
2022-11-28 08:16:43 +0100

    airtable func frontend

--- examples/3-airtable/app.js
+++ examples/3-airtable/app.js
@@ -1,1 +1,28 @@
 const result = document.querySelector('.result')
+
+const fetchData = async () => {
+  try {
+    // https://serverless-functions-adri.netlify.app
+    const { data } = await axios.get('/api/3-airtable');
+    const products = data.map((product) => {
+      const { id, url, name, price } = product;
+      return `
+        <a href="product.html?id=${id}" class="product">
+          <img src="${url}" alt="${name}"/>
+          <div class="info">
+            <h5>${name}</h5>
+            <h5 class="price">${price}</h5>
+          </div>
+        </a>
+      `
+    })
+    console.log(data);
+    result.textContent = data;
+  }
+  catch (error) {
+    result.textContent = error.message;
+    console.error(error.response.data, error.message);
+  }
+}
+
+fetchData();
