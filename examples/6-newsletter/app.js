64d7b96ee3a8a48c8c2e153f4d21fc5ddf41a914
adri-st <abasteru@emeal.nttdata.com>
2022-11-28 11:49:16 +0100

    show success, loading and error messages

--- examples/6-newsletter/app.js
+++ examples/6-newsletter/app.js
@@ -3,3 +3,18 @@ const emailInput = document.querySelector('.email-input')
 const alert = document.querySelector('.alert')
 alert.style.display = 'none'
 
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  form.classList.add('loading');
+  alert.style.display = 'none';
+  const email = emailInput.value;
+  try {
+    await axios.post('/api/6-newsletter', { email });
+    form.classList.remove('loading');
+    form.innerHTML = '<h4 class="success">Success! Please check email</h4>'
+  } catch (error) {
+    console.error(error.message);
+    alert.style.display = 'block';
+    alert.textContent = 'Something went wrong, please try again.';
+  }
+})
--- functions/6-newsletter.js
+++ functions/6-newsletter.js
@@ -7,10 +7,8 @@ const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
 
 exports.handler = async (event, context) => {
   return {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
+    headers: { 'Access-Control-Allow-Origin': '*' },
     statusCode: 200,
-    body: JSON.stringify(items)
+    body: 'JSON.stringify(items)'
   }
 }
